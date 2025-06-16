31445f774b2b850d6d7ef7662af46f984e9a1338
Kang-il Park <kangil.park@huskers.unl.edu>
2025-06-16 09:44:08 -0500

    Initial Commit

---
+++ src/objectExamples/ComboLock.java
@@ -0,0 +1,43 @@
+package objectExamples;
+
+public class ComboLock {
+	// private fields to prevent access by other classes
+	private int n1;
+	private int n2;
+	private int n3;
+	private boolean isOpen;
+	
+	/**
+	 * Constructor with no arguments (default values)
+	 */
+	public ComboLock() {
+		
+	}
+	
+	/**
+	 * Constructor with initialization of the sequence of 3 numbers
+	 */
+	public ComboLock(int n1, int n2, int n3) {
+		
+	}
+	
+	
+	/**
+	 * Set isOpen to true if correct combination is used
+	 */
+	public void open(int n1, int n2, int n3) {
+		
+	}
+	
+	
+	/**
+	 * Check if n1, n2, n3 is the correct combination, then
+	 * change values of n1, n2, n3 to the values of m1, m2, m3.
+	 * The check for correct combination may need another
+	 * helper method.
+	 */
+	public void changeCombo(int n1, int n2, int n3, int m1, int m2, int m3) {
+		
+	}
+	
+}
---
+++ src/objectExamples/ComboLockDriver.java
@@ -0,0 +1,10 @@
+package objectExamples;
+
+public class ComboLockDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
